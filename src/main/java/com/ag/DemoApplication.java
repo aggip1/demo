982c50e281ef5fae0106fe0e237427a36445f8fb
zsw <1355796120@qq.com>
2020-01-03 13:32:17 +0800

    v2.0

--- src/main/java/com/ag/DemoApplication.java
+++ src/main/java/com/ag/DemoApplication.java
@@ -8,7 +8,8 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-System.out.println("1111111111111");
+        System.out.println("1111111111111");
+        System.out.println("22222222222222");
     }
 
 }
